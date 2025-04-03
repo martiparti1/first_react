760aee33935ef46073b73c2795c6c91cd7d13dce
martiparti1 <mart2004xd@gmail.com>
2025-04-03 09:25:27 +0300

    added 2 buttons and played a bit with tailwind

--- src/DbzPlayground.jsx
+++ src/DbzPlayground.jsx
@@ -4,10 +4,21 @@ export default function DbzPlayground(){
     return(
         <>
         <div className="flex flex-col justify-center items-center min-h-screen bg-radial from-orange-300 to-orange-400">
-            <div className= "flex flex-row md:flew-col gap-20 overflow-x-auto">
+
+            <div className = 'flex flex-row gap-10'>
+                <button className="min-w-[10vw] min-h-[5vh] bg-green-500 rounded-sm mb-[5vh] cursor-pointer text-white text-bold hover:shadow-lg shadow-black-500 active:scale-95 transition-all duration-200">?Randomize?</button>
+
+                <button className="min-w-[10vw] min-h-[5vh] bg-cyan-500 rounded-sm mb-[5vh] cursor-pointer text-white text-bold hover:shadow-lg shadow-black-500 active:scale-95 transition-all duration-200">FIGHT</button>
+
+            </div>
+
+
+            <div className= "flex md:flex-row sm:flex-col flex-col gap-40">
                 <CharacterCard/>
                 <CharacterCard/>
             </div>
+
+
         </div>
         </>
     );
--- src/components/dbz/CharacterCard.jsx
+++ src/components/dbz/CharacterCard.jsx
@@ -2,8 +2,9 @@
 
 export default function CharacterCard(){
     return(
-        <div className="min-w-[300px] md:min-w-[500px] h-128 bg-white rounded-[3%] flex flex-col justify-center">
+        <div className="min-w-[60vw] min-h-[30vh] md:min-w-[25vw] md:min-h-[80vh] bg-white rounded-[3%] flex flex-col justify-center mt-[10px] md:mt-0">
             {/* hi? */}
+
         </div>
     );
 }
