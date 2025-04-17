5c069fbc55265fc9105cece9e68ca52e3551e0ab
martiparti1 <mart2004xd@gmail.com>
2025-04-17 08:51:21 +0300

    popup in progress

--- src/DbzPlayground.jsx
+++ src/DbzPlayground.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { getRandomChars, parseKi } from "./util/lib.js";
 import arrow_right from './assets/arrow_right.svg'
 import { useNavigate } from "react-router-dom";
+import { Popup } from "./util/lib.jsx";
 
 export default function DbzPlayground() {
     const [chars, setChars] = useState([]);
@@ -14,15 +15,6 @@ export default function DbzPlayground() {
         const data = await getRandomChars(2);
         setChars(data);
     };
-
-    const removeChars = () =>{
-        setChars([]);
-    }
-
-    const goHome = () =>{
-        navigate('../')
-    }
-    
 
     function handleFight(chars){
         if(parseKi(chars[0].ki) > parseKi(chars[1].ki)) alert(`${chars[0].name} wins!`)
@@ -35,14 +27,15 @@ export default function DbzPlayground() {
         <div className="flex flex-col justify-center items-center min-h-screen bg-radial from-orange-300 to-orange-400">
 
 
-            <img className= {'w-15 self-start cursor-pointer rotate-180  absolute top-5 left-5'} src={arrow_right} onClick={goHome}/>
+            <img className= {'w-15 self-start cursor-pointer rotate-180  absolute top-5 left-5'} src={arrow_right} onClick={()=> navigate('../')}/>
 
+            {<Popup visible={true}/>}
             <div className="flex flex-col-reverse md:flex-col justify-center items-center">
 
                 <div className="flex flex-col md:flex-row gap-0 md:gap-10 mt-[5vh] md:mt-0">
                     <UniversalButton bg_color={"bg-green-500"} text={'Randomize'} onClick={fetchChars} />
                     <UniversalButton bg_color={"bg-cyan-500"} text={'Fight'} onClick={()=>handleFight(chars)}/>
-                    <UniversalButton bg_color={"bg-red-500"} text={'DELETE'} onClick={removeChars} />
+                    <UniversalButton bg_color={"bg-red-500"} text={'DELETE'} onClick={()=> setChars([])} />
                 </div>
 
                 <div className="flex flex-row gap-5 md:gap-15">
--- src/util/lib.jsx
+++ src/util/lib.jsx
@@ -1,8 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function RedirectComponent({link}){
     useEffect(()=>{
         window.location.href = link;
     }, []);
     return null;
+}
+
+export function Popup({text, visible}){
+
+    const [showPopup, setshowPopup] = useState(visible);
+
+    return(
+
+        <div>
+            {showPopup && (<div className="flex flex-col bg-white w-[500px] h-[300px] rounded-[1rem] z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            
+                <button onClick={()=>setshowPopup(false)} className="justify-end">
+                        X
+                </button>
+
+            </div>)}
+        </div>
+    );
 }
